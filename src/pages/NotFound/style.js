da592180999f115c7b364e896cd1d2cb076a7da5
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-02-23 23:09:23 -0300

    reparos feitos two

--- src/pages/NotFound/style.js
+++ src/pages/NotFound/style.js
@@ -1,22 +1,23 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
-    conatiner: {
-        width: "100vw",
-        height: "100vh"
-    },
-    image: {
-        width: "100vw",
-        height: "30vh",
-        backgroundImage: "url(./assets/Reuss.png)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover"
-    },
-    text: {
-        backgroundColor: "#fff",
-        textAlign: "center",
-        paddingTop: "50px",
-        width: "100vw",
-        height: "70vh"      
-    }
-})
+  conatiner: {
+    width: "100vw",
+    height: "100vh",
+  },
+  image: {
+    width: "100vw",
+    height: "30vh",
+    background:
+      "linear-gradient(0deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+  text: {
+    backgroundColor: "#fff",
+    textAlign: "center",
+    paddingTop: "50px",
+    width: "100vw",
+    height: "70vh",
+  },
+});
